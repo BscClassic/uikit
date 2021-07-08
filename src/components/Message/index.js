30c0723b08c072b9952726a331bb2bddeefd1d40
BscClassic <ah0229247@gmail.com>
2021-07-09 01:02:26 +0700

    uikit file

---
+++ src/components/Message/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Message = void 0;
+var Message_1 = require("./Message");
+Object.defineProperty(exports, "Message", { enumerable: true, get: function () { return Message_1.default; } });
+//# sourceMappingURL=index.js.map
