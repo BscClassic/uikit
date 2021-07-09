ceb30a44e8a621fdd38681a6a13e5f304391678e
BscClassic <ah0229247@gmail.com>
2021-07-09 16:24:02 +0700

    commit file

--- src/components/FallingBunnies/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FallingBunnies = void 0;
-var FallingBunnies_1 = require("./FallingBunnies");
-Object.defineProperty(exports, "FallingBunnies", { enumerable: true, get: function () { return FallingBunnies_1.default; } });
-//# sourceMappingURL=index.js.map
